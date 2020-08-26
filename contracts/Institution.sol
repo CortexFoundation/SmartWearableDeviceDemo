abd0ab7e8597f426b3252dce6b24e6cc1c5d2f23
Li-Qing <liqingnz@gmail.com>
2020-08-26 15:28:30 +0800

    added api

--- contracts/Institution.sol
+++ contracts/Institution.sol
@@ -90,6 +90,7 @@ contract Institution is Ownable {
     }
 
 // -------------------------- Service Purchase ---------------------------------
+    function checkForAvailbleServices(address _userAddr) public;
     
     function purchaseService(uint256 _serviceIndex)
       public payable;
